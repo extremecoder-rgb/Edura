e38e67d0c0eecf73540c5b7249571e0757105494
extremecoder-rgb <hsuswiowkskow@gmail.com>
2025-08-10 10:20:06 +0530

    configured router dom for navigation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <>
+            {/* create route to connect with home.jsx, signup.jsx, login.jsx */}
+        </>
+    )
+}
+
+export default App
