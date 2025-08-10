0707ee9ee2d54cebeb4db23e7c5439f6befcdc8f
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-10 12:01:04 +0530

    UserSchema defined initially

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,5 +1,42 @@
 //create user model ->
 
-//create a user schema include name, description, email, password, role(student,educator), photoUrl, enrolled courses
+//create a user schema include name, about, email, password, role(student,educator), photoUrl, enrolled courses
 
-//export 
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    about: {
+      type: String,
+      default: "",
+    },
+    email: {
+      type: String,
+      unique: [true, "Email should be unique"],
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
+    },
+    role: {
+      type: String,
+      enum: ["student", "educator"],
+      default: "student",
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://imgs.search.brave.com/pkPyTQFTOVFQw7Hki6hg6cgY5FPZ3UzkpUMsnfiuznQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/dmVjdG9yc3RvY2su/Y29tL2kvNTAwcC80/MS85MC9hdmF0YXIt/ZGVmYXVsdC11c2Vy/LXByb2ZpbGUtaWNv/bi1zaW1wbGUtZmxh/dC12ZWN0b3ItNTcy/MzQxOTAuanBn",
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model("User", userSchema);
+export default User;
+//export
