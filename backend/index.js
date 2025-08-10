d7aba680c64db66caf373db60117251b9b1028a0
extremecoder-rgb <hsuswiowkskow@gmail.com>
2025-08-10 11:21:06 +0530

    Express server created successfully

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
 //create express server here ->
+
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const port = process.env.PORT
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+
+app.listen(port, ()=>{
+    console.log("Server started")
+})
