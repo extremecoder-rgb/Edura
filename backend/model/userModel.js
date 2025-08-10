ebf5700f5dd9a8e6e70e10ff38e13c05e94e797f
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-10 12:03:31 +0530

    connected to courseSchema

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -34,9 +34,15 @@ const userSchema = new mongoose.Schema(
       default:
         "https://imgs.search.brave.com/pkPyTQFTOVFQw7Hki6hg6cgY5FPZ3UzkpUMsnfiuznQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/dmVjdG9yc3RvY2su/Y29tL2kvNTAwcC80/MS85MC9hdmF0YXIt/ZGVmYXVsdC11c2Vy/LXByb2ZpbGUtaWNv/bi1zaW1wbGUtZmxh/dC12ZWN0b3ItNTcy/MzQxOTAuanBn",
     },
+    enrolledCourses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
   },
   { timestamps: true }
 );
 const User = mongoose.model("User", userSchema);
+
 export default User;
-//export
