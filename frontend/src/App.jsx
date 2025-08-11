03d9a7bfc306fa3c63205d4d51d0165dabdd7dc2
extremecoder-rgb <hsuswiowkskow@gmail.com>
2025-08-11 13:41:50 +0530

    Secured user routes successfully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import {Navigate, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import Login from './pages/Login'
@@ -7,17 +7,21 @@ import Landing from './pages/Landing';
 
 import {ToastContainer} from "react-toastify"
 import getCurrentUser from './customHooks/getCurrentUser'
+import { useSelector } from 'react-redux'
+import Profile from './pages/Profile'
 export const serverUrl = "http://localhost:8000"
 function App() { 
   getCurrentUser()
+  const {userData} = useSelector(state=>state.user)
   return (
     <>
     <ToastContainer />
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/signin" element={<Login/>} />
-          <Route path="/signup" element={<SignUp />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/signup" element={!userData ? <SignUp />: <Navigate to={"/"} />} />
+          <Route path="/signin" element={<Login/>} />
+          <Route path="/profile" element={userData ? <Profile /> :<Navigate to={"/signup"}/>} />
         </Routes>
     </>
   )
