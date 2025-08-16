2abe79ee35b47067bd308577da559910a0bde4f4
extremecoder-rgb <hsuswiowkskow@gmail.com>
2025-08-16 22:51:58 +0530

    complete total earning logic and admin panel done

--- frontend/src/pages/Educator/Dashboard.jsx
+++ frontend/src/pages/Educator/Dashboard.jsx
@@ -2,10 +2,30 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { FaArrowLeftLong } from "react-icons/fa6";
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
 
 function Dashboard() {
   const { userData } =useSelector(state=>state.user)
   const navigate = useNavigate()
+  const {creatorCourseData} = useSelector(state=>state.course)
+
+  const CourseProgressData = creatorCourseData?.map((course)=>({
+    name:course.title?.slice(0,10) + "...",
+    lectures: course.lectures?.length || 0
+  })) || [];
+
+  const EnrollData = creatorCourseData?.map((course)=>({
+    name:course.title?.slice(0,10) + "...",
+    enrolled: course.enrolledStudents?.length || 0
+  })) || [];
+
+  const totalEarning = creatorCourseData?.reduce((sum,course)=>{
+    const studentCount = course.enrolledStudents?.length || 0;
+    const courseRevenue = course.price ? course.price * studentCount : 0
+
+    return sum + courseRevenue;
+  }, 0) || 0
+
   return (
     <div className='flex min-h-screen bg-gray-100'>
       <FaArrowLeftLong className='w-[22px] absolute top-[10%] left-[5%] h-[22px] cursor-pointer' onClick={()=>navigate("/home")}/>
@@ -17,15 +37,38 @@ function Dashboard() {
             <h1 className='text-2xl font-bold text-gray-800'>Welcome ,{userData?.name || "Educator"} 👋
             </h1>
 
-            <h1 className='text-xl font-semibold text-gray-800'>Total Earning : 0</h1>
+            <h1 className='text-xl font-semibold text-gray-800'>Total Earning : ₹{totalEarning.toLocaleString()}</h1>
             <p className='text-gray-600 text-sm'>{userData?.description || "Start Creating Courses for Your Students"}</p>
 
             <h1 className='px-[10px] text-center py-[10px] border-2 bg-black border-black text-white rounded-[10px] text-[15px] font-light flex items-center justify-center cursor-pointer' onClick={()=>navigate("/courses")}>Create Courses</h1>
           </div>
         </div>
         {/* Graph section */}
-        <div>
+        <div className='bg-white rounded-lg shadow-md p-6'>
+          <h2 className='text-lg font-semibold mb-4'>Course Progress (Lectures)</h2>
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart data={CourseProgressData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name"/>
+              <YAxis/>
+              <Tooltip/>
+              <Bar dataKey="lectures" fill='black' radius={[5,5,0,0]}/>
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
 
+        {/* Enrolled data */}
+        <div className='bg-white rounded-lg shadow-md p-6'>
+          <h2 className='text-lg font-semibold mb-4'>Students Enrollment</h2>
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart data={EnrollData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name"/>
+              <YAxis/>
+              <Tooltip/>
+              <Bar dataKey="enrolled" fill='black' radius={[5,5,0,0]}/>
+            </BarChart>
+          </ResponsiveContainer>
         </div>
 
 
